ceb810441daea9f96b3317868ff32aa991c52cb0
Ronald Alvin <ronaldalvin@Ronalds-MacBook-Pro.local>
2025-10-07 20:06:55 +0800

    Implemented requestLogger with excution time and path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const tasksRouter = require("./routes/tasks");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import tasksRouter from "./routes/tasks.js";
+import { requestLogger } from "./middleware/logger.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(requestLogger);
 
 // Use tasks router
 app.use("/", tasksRouter);
